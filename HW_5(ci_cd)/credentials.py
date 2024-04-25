d76761a244214b9a8a35f9abc57997f664c8ea7c
Oleksandra_Bereza <bluckyalex1@gmail.com>
2024-04-25 23:56:10 +0200

    new fixes

--- HW_5(ci_cd)/credentials.py
+++
@@ -1,7 +0,0 @@
-database = 'TRN'
-user = 'oleksandraLogin'
-password = 'StrongPassword1@'
-server='host.docker.internal'
-# server = 'EP\\SQLEXPRESS'
-port = 1433
-driver = 'ODBC Driver 17 for SQL Server'
